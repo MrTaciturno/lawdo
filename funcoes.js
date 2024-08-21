12d5586fabf2b45758e987086292de7b248951d4
MrTaciturno <leo.age@gmail.com>
2024-08-21 17:22:25 -0300

    Update funcoes.js

--- funcoes.js
+++ funcoes.js
@@ -82,6 +82,8 @@ function criaTesteDOCX(textoLaudo, formatacao, nome){
             }],
         });
 
+
+
         // Gerar e baixar o arquivo DOCX
         docx.Packer.toBlob(doc).then(blob => {
             saveAs(blob, nome+".docx");
@@ -166,7 +168,7 @@ function montaLaudo(e){
 
     }
 
-
+    iT++; aL.push(iT + " - Do Levantamento Fotográfico"); nF[aL.length-1]=1;//título do levantamento fotográfico
 
 
 //  finalização do documento
@@ -612,7 +614,7 @@ var opcoesDeMaquinas = [
     { texto: 'Real Web', acao: 'Real Web.' },
     { texto: 'Positivo', acao: 'Quando da chegada desta equipe pericial, os noteiros encontravam-se fechados e as máquinas desligadas. Quando solicitado o funcionamento destas, as máquinas exibiram em suas telas jogo eletrônico. As máquinas foram abertas, encontrando-se a quantia total de R$XXX,00, a qual foi entregue em mãos ao representante da Polícia Civil. Os noteiros foram removidos de seus gabinetes e inutilizados no próprio local. Os dispositivos de armazenamento, a saber, XXXXX cartões de memória, XXXXX discos rígidos, XXXXX pendrives, XXXXX placas contendo memória programável (EPROM) foram removidos e acondicionados em embalagem plástica lacrada sob o número SPTC LACRE.\r\nConsiderações Finais\r\nFoi realizado o exame de funcionamento das referidas máquinas no local, sendo que XXXXX máquinas possuíam jogo eletrônico sorteador de resultados, na qual, o ganho ou perda independe da habilidade física ou mental do agente, ou seja, depende exclusivamente da sorte (“Jogo de Azar”).'},
     
-    { texto: 'Negativo', acao: 'Durante o exame, esse relator acessou tela que solicitava a inserção de senha para prosseguimento, no entanto, a mesma não foi fornecida pelo responsável e a continuação do exame in loco ficou prejudicada. Não foi possível acessar o histórico de acesso dos navegadores das máquinas.\r\nConsiderações Finais\r\nPor último, cumpre-se consignar que XXXXX máquinas World Link/Totem Brasil possuíam, no momento dos exames periciais, acesso à internet. Dessa forma, não foi possível descartar a hipótese que outrora as máquinas poderiam estar sendo usadas para acessar sites/servidores contendo jogos sorteadores de resultado envolvendo a perda e ganho de dinheiro.\r\nApesar de não ter sido possível constatar o jogo de azar, essas máquinas possuem semelhanças e componentes eletrônicos de mesmas características de outras máquinas que apresentaram esse tipo de jogo em outras ocasiões.\r\nAtendendo a OS-03/2022 - NPC Americana, que revogou a OS-01/2022, os dispositivos de armazenamento de memória, os noteiros e valores porventura existentes nas máquinas deixaram de ser retirados ou apreendidos.' }
+    { texto: 'Negativo', acao: 'Durante o exame, esse relator acessou tela que solicitava a inserção de senha para prosseguimento, no entanto, a mesma não foi fornecida pelo responsável e a continuação do exame in loco ficou prejudicada. Não foi possível acessar o histórico de acesso dos navegadores das máquinas.\r\nConsiderações Finais\r\nPor último, cumpre-se consignar que XXXXX máquinas World Link/Totem Brasil possuíam, no momento dos exames periciais, acesso à internet. Dessa forma, não foi possível descartar a hipótese que outrora as máquinas poderiam estar sendo usadas para acessar sites/servidores contendo jogos sorteadores de resultado envolvendo a perda e ganho de dinheiro.\r\nApesar de não ter sido possível constatar o jogo de azar, essas máquinas possuem semelhanças e componentes eletrônicos de mesmas características de outras máquinas que apresentaram esse tipo de jogo em outras ocasiões.\r\nAtendendo a OS-03/2022 - NPC Americana, que revogou a OS-01/2022, os dispositivos de armazenamento, os noteiros e valores porventura existentes nas máquinas deixaram de ser retirados ou apreendidos.' }
     
 ];
 
