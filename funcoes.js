06b74b5eb2d29cf97f5703d99569bf47cebabcee
MrTaciturno <leo.age@gmail.com>
2024-08-24 08:57:42 -0300

    Update funcoes.js

--- funcoes.js
+++ funcoes.js
@@ -171,11 +171,11 @@ function criaTesteDOCX(textoLaudo, formatacao, nome){
                             },
                             children: [
                                 new docx.TextRun({
-                                    text: "\tEste laudo foi elaborado em "+ docx.PageNumber.TOTAL_PAGES +" páginas com cópia digital arquivada no Sistema Gestor de Documentos e Laudos da Superintendência da Polícia Técnico-Científica do Estado de São Paulo (Portaria SPTC 145/2012).",
-                                    font: 'Arial',
-                                    size: 24,
-                                    bold: false,
-                                    // children:["Este laudo foi elaborado em ", docx.PageNumber.TOTAL_PAGES, " páginas com cópia digital arquivada no Sistema Gestor de Documentos e Laudos da Superintendência da Polícia Técnico-Científica do Estado de São Paulo (Portaria SPTC 145/2012)."],
+                                    // text: "\tEste laudo foi elaborado em "+ docx.PageNumber.TOTAL_PAGES +" páginas com cópia digital arquivada no Sistema Gestor de Documentos e Laudos da Superintendência da Polícia Técnico-Científica do Estado de São Paulo (Portaria SPTC 145/2012).",
+                                    // font: 'Arial',
+                                    // size: 24,
+                                    // bold: false,
+                                    children:["Este laudo foi elaborado em ", docx.PageNumber.TOTAL_PAGES, " páginas com cópia digital arquivada no Sistema Gestor de Documentos e Laudos da Superintendência da Polícia Técnico-Científica do Estado de São Paulo (Portaria SPTC 145/2012)."],
                                 }),
                             ],
                         }),
