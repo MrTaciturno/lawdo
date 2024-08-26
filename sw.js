2abc664a1e14d8d28b67ee997beb072906d70715
MrTaciturno <leo.age@gmail.com>
2024-08-26 06:42:12 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,9 +4,9 @@ const urlsToCache = [
     '/index.html',
     '/style.css',
     '/funcoes.js',
-    "https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.5/FileSaver.min.js",
-    "https://unpkg.com/docx@7.1.0/build/index.js",
-    "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.9.359/pdf.min.js"
+    'https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.5/FileSaver.min.js',
+    'https://unpkg.com/docx@7.1.0/build/index.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.9.359/pdf.min.js'
     // Adicione aqui outros recursos que devem ser armazenados em cache
 ];
 
